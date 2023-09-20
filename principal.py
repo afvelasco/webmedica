c20d8724c0ba439014b52a76acbd4229dea03d27
Andres Velasco <afvelasco@gmai.com>
2023-09-20 16:51:23 -0500

    2323

--- principal.py
+++ principal.py
@@ -45,9 +45,9 @@ def pacientes():
 # información: documento, nombre, nacimiento, estatura,
 # peso, tiposangre, edad, imc
 
-app.route("/agregarpaciente")
+@app.route("/agregarpaciente")
 def agregarpaciente():
-    render_template("agregarpaciente.html")
+    return render_template("agregarpaciente.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port="9000", debug=True)
